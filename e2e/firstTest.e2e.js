c68e5910a3d99f356c2c43455779020d6b5657d6
O.Oualha <oo@demodern.de>
2022-11-03 12:56:31 +0100

    - eject to bareworkflow - add detox

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,27 @@
+describe("Example", () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    // await device.reloadReactNative();
+  });
+
+  it("should have intro screen", async () => {
+    await expect(element(by.text("Log in"))).toBeVisible();
+    await element(by.text("Log in")).tap();
+    await expect(element(by.id("webview"))).toBeVisible();
+    // await element(by.text("Log in")).tap();
+    // await expect(element(by.text("Password"))).toBeVisible();
+  });
+
+  // it('should show hello screen after tap', async () => {
+  //   await element(by.id('hello_button')).tap();
+  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+  // });
+
+  // it('should show world screen after tap', async () => {
+  //   await element(by.id('world_button')).tap();
+  //   await expect(element(by.text('World!!!'))).toBeVisible();
+  // });
+});
